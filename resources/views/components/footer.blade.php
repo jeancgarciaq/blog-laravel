7fcd19cab86d5c01f4f1d8fa8c161f7753280865
jeancgarciaq <jcvictory@hotmail.com>
2025-07-13 15:07:07 -0400

    add: Vista componente footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="bg-white shadow mt-8">
+    <div class="container mx-auto px-4 py-6">
+        <div class="flex justify-between items-center">
+            <p>© 2025 Jean Carlo Garcia. All rights reserved.</p>
+            <div>
+                <a href="#" class="text-gray-600 hover:text-gray-800">Contact</a>
+                <a href="#" class="ml-4 text-gray-600 hover:text-gray-800">FAQs</a>
+            </div>
+        </div>
+    </div>
+</footer>
