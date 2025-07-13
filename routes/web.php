15e3a503c5d4d30673c6aeb816c28e8bd85c0a90
jeancgarciaq <jcvictory@hotmail.com>
2025-07-13 15:01:14 -0400

    feat: Ajuste rutas a nuevas estructura

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\HomePage;
+use App\Livewire\ShowPost;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class)->name('home');
+
+Route::get('/post/{id}', ShowPost::class)->name('posts.show');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
+
