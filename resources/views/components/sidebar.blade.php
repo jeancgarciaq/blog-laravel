b0b5d3be01940b40d2de94919cae7a13164e8de1
jeancgarciaq <jcvictory@hotmail.com>
2025-07-13 15:08:20 -0400

    add: Vista componente sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="p-4">
+    <h3 class="text-lg font-bold mb-4">Categories</h3>
+    <ul>
+        {{-- Loop through your categories here --}}
+        <li class="mb-2"><a href="#" class="text-gray-600 hover:text-gray-800">Category 1</a></li>
+        <li class="mb-2"><a href="#" class="text-gray-600 hover:text-gray-800">Category 2</a></li>
+        <li class="mb-2"><a href="#" class="text-gray-600 hover:text-gray-800">Category 3</a></li>
+    </ul>
+</div>
